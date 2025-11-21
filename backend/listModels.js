43bb8a9d9d98a3083bbab073e8e830aaf36ee46d
fawzaanshaikh <fawzi20112001@gmail.com>
2025-11-21 22:41:07 +0530

    Removed listModels.js

--- backend/listModels.js
+++
@@ -1,9 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
-const main = async () => {
-  const response = await genAI.listModels();
-  console.log(JSON.stringify(response, null, 2));
-};
-
-main();
